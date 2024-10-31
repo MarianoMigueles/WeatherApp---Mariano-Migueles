5d15be519b12914d9008c765929e6de2bd069d97
Mariano <164581983+MarianoMigueles@users.noreply.github.com>
2024-10-31 14:14:34 -0300

    Update

--- app/src/main/java/com/example/myweatherapp/Ciudad/CiudadViewModel.kt
+++ app/src/main/java/com/example/myweatherapp/Ciudad/CiudadViewModel.kt
@@ -4,18 +4,23 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
-class CiudadViewModel : ViewModel() {
+class CiudadViewModel() : ViewModel() {
     var estado by mutableStateOf<CiudadEstado>(CiudadEstado.Vacio)
 
     fun ejecutar(intencion: CiudadIntencion) {
         when(intencion) {
-            is CiudadIntencion.Actualizar -> actualizar()
+            is CiudadIntencion.Actualizar -> cargarCiudades()
         }
     }
 
-    private fun actualizar() {
+    private fun cargarCiudades() {
         estado = CiudadEstado.Cargando
+        viewModelScope.launch {
+
+        }
         estado = CiudadEstado.Error("No funciono el servidor, esta todo roto")
     }
 }
